96484e84c6c781885a8bafc1b4a43af29cc38dbe
alplky <alicia.pelkey@gmail.com>
2020-11-20 17:01:54 -0600

    this isn't working, going to try other options

--- UFO-Level-2/static/js/app_bonus.js
+++ UFO-Level-2/static/js/app_bonus.js
@@ -1,34 +1,54 @@
-const tableData = data
-const filters = {}
+// function to show data 
+function tableData(data) {
+    
+    let table = d3.select("#ufo-table")
 
-function buildTable() {
-    const tbody = d3.select('tbody')
-    tbody.html('')
-    data.forEach(dataRow => {
-        const elements = tbody.append('tr')
-        Object.values(dataRow).forEach(value => tbody.append('td').text(value))
+    table.selectAll("tr").remove()
+    
+    // set up header row
+    let headerRow = table.append("tr")
+
+    headerRow.append("th").text("Datetime")
+    headerRow.append("th").text("City")
+    headerRow.append("th").text("State")
+    headerRow.append("th").text("Country")
+    headerRow.append("th").text("Shape")
+    headerRow.append("th").text("Duration (min)")
+    headerRow.append("th").text("Comments")
+
+    // set up table body
+    let tableBody = table.append("tbody")
+
+    data.forEach((d) => {
+        let row = tableBody.append("tr")
+        row.append("td").text(d.datetime)
+        row.append("td").text(d.city)
+        row.append("td").text(d.state)
+        row.append("td").text(d.country)
+        row.append("td").text(d.shape)
+        row.append("td").text(d.durationMinutes)
+        row.append("td").text(d.comments)
     })
+
 }
 
-function updateFilters() {
-    const changedElement = d3.select(this).select("input");
-    const elementValue = changedElement.property("value");
-    const filterId = changedElement.attr("id");
+// create function to filter by date input
+function handleClick(input) {
+    let dateInput = d3.select("#datetime").property("value")
+    let cityInput = d3.select("#city").property("value")
+    let stateInput = d3.select("#state").property("value")
+    let countryInput = d3.select("#country").property("value")
+    let shapeInput = d3.select("#shape").property("value")
 
-    elementValue ? filters[filterId] = elementValue : delete filters[filterId]
+    let filteredData = data.filter((d) => {
+       if input (d.date === dateInput && d.city === cityInput && d.state === stateInput && d.country === countryInput && d.shape === shapeInput &&) {
+            return input
+        }
+    }) 
 
-    filterTable()
+    tableData(filteredData)
 }
 
-function filterTable() {
-    let filteredData = tableData
+// pass id for button and function to click
+d3.select("#filter-btn").on("click", handleClick)
 
-    Object.entries(filters).forEach(([key, value]) => {
-        filteredData = filteredData.filter(row => row[key] === value)
-    })
-
-    buildTable(filteredData)
-}
-
-d3.selectAll('.filter').on('change', updateFilters)
-buildTable()
