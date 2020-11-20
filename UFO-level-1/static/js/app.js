5511be2797c051236821de9a8aa10d0c84e55f5d
alplky <alicia.pelkey@gmail.com>
2020-11-20 13:10:15 -0600

    set up table body

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,4 +14,17 @@ function tableData(data) {
     headerRow.append("th").text("Duration (min)")
     headerRow.append("th").text("Comments")
 
+    let tableBody = table.append("tbody")
+
+    data.forEach((d) => {
+        let row = tableBody.append("tr")
+        row.append("td").text(d.datetime)
+        row.append("td").text(d.city)
+        row.append("td").text(d.state)
+        row.append("td").text(d.country)
+        row.append("td").text(d.shape)
+        row.append("td").text(d.durationMinutes)
+        row.append("td").text(d.comments)
+    })
+
 }
